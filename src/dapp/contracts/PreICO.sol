abd9e4a52f4c5ee406d8f121457759677699f48c
vagrant <vagrant@vagrant.vm>
2017-08-26 00:48:50 +0000

    It inits OK

---
+++ src/dapp/contracts/PreICO.sol
@@ -0,0 +1,46 @@
+pragma solidity ^0.4.4;
+
+//import "./ERC20.sol";
+
+contract PreICO  { //is ERC20 {
+    address public owner;
+
+    uint8 public constant decimals = 18;
+    uint256 _totalSupply = 1000000;
+
+    mapping(address => uint256) balances;
+
+    mapping(address => mapping (address => uint256)) allowed;
+
+    function PreICO() {
+        owner = msg.sender;
+        balances[owner] = _totalSupply;
+    }
+
+    function totalSupply() constant returns (uint totalSupply) {
+        totalSupply = _totalSupply;
+    }
+
+    function balanceOf(address _owner) constant returns (uint balance) {
+        balance = balances[_owner];
+    }
+
+    function transfer(address _to, uint _value) returns (bool success) {
+        revert();
+    }
+
+    function transferFrom(address _from, address _to, uint _value) returns (bool success) {
+        revert();
+    }
+
+    function approve(address _spender, uint _value) returns (bool success) {
+        revert();
+    }
+
+    function allowance(address _owner, address _spender) constant returns (uint remaining) {
+        revert();
+    }
+    
+  //  event Transfer(address indexed _from, address indexed _to, uint _value);
+  //  event Approval(address indexed _owner, address indexed _spender, uint _value);
+}
---
+++ src/dapp/test/PreICO.js
@@ -0,0 +1,19 @@
+var PreICO = artifacts.require("../contracts/PreICO.sol");
+
+contract("PreICO", function(accounts) {
+    it("should compile and deploy!!", function(){
+        var ownerBalance = 0;
+        var owner;
+        var ico; 
+        return PreICO.deployed().then(function(instance) {
+            ico = instance;
+            return instance.balanceOf.call(accounts[0]);
+        }).then(function(balance) {
+            ownerBalance = balance;
+            return ico.owner.call();
+        }).then(function(owner) {
+            assert.equal(ownerBalance.valueOf(), 1000000, "1000000 wasn't the owners balance.");
+            assert.equal(owner.valueOf(), accounts[0], "The owner wasn't set to the correct person.");
+        }); 
+    });
+});
