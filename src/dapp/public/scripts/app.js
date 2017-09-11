96d79155b8d6f728988fc9dc764302eaf285b2af
stanacton <stanacton@gmail.com>
2017-09-11 21:45:37 +0100

    Added new files and style

--- src/dapp/public/scripts/app.js
+++ src/dapp/public/scripts/app.js
@@ -20,6 +20,22 @@ app.config(function ($routeProvider, $locationProvider) {
             templateUrl: "partials/home.html",
             controller: "HomeCtrl"
         })
+        .when("/dashboard", {
+            templateUrl: "partials/dashboard.html",
+            controller: "HomeCtrl"
+        })
+        .when("/trading", {
+            templateUrl: "partials/trading.html",
+            controller: "HomeCtrl"
+        })
+        .when("/voting", {
+            templateUrl: "partials/voting.html",
+            controller: "HomeCtrl"
+        })
+        .when("/wallet", {
+            templateUrl: "partials/wallet.html",
+            controller: "HomeCtrl"
+        })
         .otherwise({
             redirectTo: "/home"
         });
