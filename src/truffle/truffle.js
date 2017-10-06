747f3bf7be550c2541908ada915633766f189395
stanacton <stanacton@gmail.com>
2017-10-06 03:48:40 +0100

    Demo deployed

--- src/truffle/truffle.js
+++ src/truffle/truffle.js
@@ -1,8 +1,8 @@
 module.exports = {
   networks: {
     development: {
-      host: "localhost",
-      port: 8545,
+      host: "testnet.dpactum.io",
+      port: 5545,
       network_id: "*" // Match any network id
     },
     "staging": {
