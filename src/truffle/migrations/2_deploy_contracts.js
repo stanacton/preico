31e4d5fc7caff8fc97f5182babcbcf86eab79563
stanacton <stanacton@gmail.com>
2017-10-13 12:01:56 +0100

    remove unused files

--- src/truffle/migrations/2_deploy_contracts.js
+++ src/truffle/migrations/2_deploy_contracts.js
@@ -1,20 +1,13 @@
 var PreICO = artifacts.require("./PreICO.sol");
-var DevBank = artifacts.require("./DevBank.sol");
 var path = require("path");
 
 var fs = require("fs");
 
 module.exports = function(deployer) {
-  deployer.deploy(DevBank);
-  
   deployer.deploy(PreICO, 1000000, web3.toWei(2,"ether")).then(function(err, result) {
     var addressData = {};
     addressData.PreICO = {
       address: PreICO.address
-    };
-
-    addressData.DevBank = {
-      address: DevBank.address
     };
 
     var txt = JSON.stringify(addressData);
@@ -26,13 +19,6 @@ module.exports = function(deployer) {
       fs.createReadStream(preicopath).pipe(fs.createWriteStream('../dapp/public/PreICO.json'));
     } else {
       console.error("PreICO.json wasn't deployed");
-    }
-
-    var devbankpath = path.join(__dirname, "../build/contracts/DevBank.json");
-    if (fs.existsSync(devbankpath)) {
-      fs.createReadStream(devbankpath).pipe(fs.createWriteStream('../dapp/public/DevBank.json'));
-    } else {
-      console.error("DevBank.json wasn't deployed.");
     }
   });
 };
